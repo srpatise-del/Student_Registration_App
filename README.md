c2ba7cd8361a00f350256e22bcdf7249bb34ae4d
sarun <sp.patise@gmail.com>
2026-02-17 00:05:44 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_student_registration_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_student_registration_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_student_registration_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
